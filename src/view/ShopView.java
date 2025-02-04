6f14c5e5e89e24b09d630341e2871f3c2ad888fe
krschan <krischxn@gmail.com>
2025-02-04 19:50:07 +0100

    REFACTOR(ShopView): Simplify UI setup and key event handling

--- src/view/ShopView.java
+++ src/view/ShopView.java
@@ -5,14 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import main.Shop;
-import model.Product;
 
 import java.awt.GridLayout;
 import javax.swing.JLabel;
